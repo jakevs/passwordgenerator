383bbd623c6dc505acb3e46f17066cfaf070e1fc
James Van Slyke <jakevs3@gmail.com>
2020-03-29 18:38:15 -0500

    getting rid of nonessential code

--- script.js
+++ script.js
@@ -37,18 +37,18 @@ var specialCh = "!@#$%^&*()[]{}?";
 
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
+// }
 
-}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); {
-}
+// generateBtn.addEventListener("click", writePassword); {
+// }
 
 document.getElementById("generate").onclick = function(event) {
 //create variable
